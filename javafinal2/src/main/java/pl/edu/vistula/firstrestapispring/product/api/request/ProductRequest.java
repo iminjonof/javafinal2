21de312ed9f1e406cd12e45695c21e62e9deb99c
iminjonof <89642483+iminjonof@users.noreply.github.com>
2026-01-15 19:30:05 +0100

    Add files via upload

---
+++ javafinal2/src/main/java/pl/edu/vistula/firstrestapispring/product/api/request/ProductRequest.java
@@ -0,0 +1,16 @@
+package pl.edu.vistula.firstrestapispring.product.api.request;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ProductRequest {
+
+    private final String name;
+
+    @JsonCreator
+    public ProductRequest(@JsonProperty ("name") String name) { this.name = name; }
+
+    public String  getName() { return name; }
+}
+
